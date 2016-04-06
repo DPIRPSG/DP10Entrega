50c31d9b9fccb7f2fe9f372defbe3a8e1ba81e25
ManuelLR <ManuelLR@github.com>
2016-04-06 17:13:02 +0200

    Domain Code - Revised

--- WorkSpace/Acme-Barter/src/main/java/domain/Item.java
+++ WorkSpace/Acme-Barter/src/main/java/domain/Item.java
@@ -6,8 +6,6 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -52,28 +50,5 @@ public class Item extends DomainEntity{
 
 
 	// Relationships ----------------------------------------------------------
-	private Collection<Barter> x;
-	private Collection<Barter> y;
-	
-	
-	@Valid
-	@NotNull
-	@OneToMany(mappedBy="offered")
-	public Collection<Barter> getX() {
-		return x;
-	}
-	public void setX(Collection<Barter> x) {
-		this.x = x;
-	}
-	
-	@Valid
-	@NotNull
-	@OneToMany(mappedBy="requested")
-	public Collection<Barter> getY() {
-		return y;
-	}
-	public void setY(Collection<Barter> y) {
-		this.y = y;
-	}
 
 }
