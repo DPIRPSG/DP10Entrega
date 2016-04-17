6bb7fa999baf2241b54dc9c8def0dba925f07730
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2016-04-17 19:11:20 +0200

    Todos los tests y pequeño cambio en Populate
    -Cambio del populate: match2 ya no tiene Auditor asignado ni report
    escrito.

--- WorkSpace/Acme-Barter/src/test/java/services/UserServiceTest.java
+++ WorkSpace/Acme-Barter/src/test/java/services/UserServiceTest.java
@@ -1,6 +1,5 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
@@ -21,8 +20,10 @@ import security.UserAccount;
 import security.UserAccountService;
 import utilities.AbstractTest;
 import domain.Actor;
+import domain.Administrator;
 import domain.Barter;
 import domain.Match;
+import domain.SocialIdentity;
 import domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,7 +37,7 @@ public class UserServiceTest extends AbstractTest {
 	// Service under test -------------------------
 
 	@Autowired
-	private UserService customerService;
+	private UserService userService;
 	
 	// Other services needed -----------------------
 	
@@ -47,20 +48,26 @@ public class UserServiceTest extends AbstractTest {
 	private ActorService actorService;
 	
 	@Autowired
+	private SocialIdentityService socialIdentityService;
+	
+	@Autowired
 	private BarterService barterService;
 	
 	@Autowired
 	private MatchService matchService;
 	
+	@Autowired
+	private AdministratorService administratorService;
+	
 	// Tests ---------------------------------------
 	
 	/**
-	 * Acme-Six-Pack - Level C - 8.1
-	 * Register to the system as a customer.
+	 * Acme-Barter - Level C - 9.1
+	 * Register to the system as a user.
 	 */
 	
 	/**
-	 * Positive test case: Registrarse como Customer
+	 * Positive test case: Registrarse como User
 	 * 		- Acción
 	 * 		+ Entrar el registro como anónimo
 	 * 		+ Rellenar los campos
@@ -74,12 +81,12 @@ public class UserServiceTest extends AbstractTest {
 	 */
 	
 	@Test 
-	public void testNewCustomer() {
+	public void testNewUser() {
 		// Declare variables
-		User customer;
+		User user;
 		UserAccount userAccount;
-		User customerRegistered;
-		Actor authenticatedCustomer;
+		User userRegistered;
+		Actor authenticatedUser;
 		
 		// Load objects to test
 		
@@ -88,38 +95,38 @@ public class UserServiceTest extends AbstractTest {
 		
 		
 		// Execution of test
-		customer = customerService.create();
+		user = userService.create();
 		
-		customer.setName("Nuevo");
-		customer.setSurname("Customer");
-		customer.setPhone("123456789");
+		user.setName("Nuevo");
+		user.setSurname("User");
+		user.setPhone("123456789");
 
-		userAccount = userAccountService.create("CUSTOMER");
+		userAccount = userAccountService.create("USER");
 		
-		userAccount.setUsername("nuevoCustomer");
-		userAccount.setPassword("nuevoCustomer");
+		userAccount.setUsername("nuevoUser");
+		userAccount.setPassword("nuevoUser");
 		
-		customer.setUserAccount(userAccount);
+		user.setUserAccount(userAccount);
 		
-		customerRegistered = customerService.saveFromOtherService(customer);
+		userRegistered = userService.saveFromOtherService(user);
 		
 		// Checks results
 		authenticate("admin");
-		Assert.isTrue(customerService.findAll().contains(customerRegistered), "El customer nuevo registrado no se encuentra entre los customers registrados en el sistema."); // First check
+		Assert.isTrue(userService.findAll().contains(userRegistered), "El user nuevo registrado no se encuentra entre los users registrados en el sistema."); // First check
 		unauthenticate();
 		
-		authenticate("customer1");
+		authenticate("nuevoUser");
 		
-		authenticatedCustomer = actorService.findByPrincipal();
+		authenticatedUser = actorService.findByPrincipal();
 		
-		Assert.notNull(authenticatedCustomer, "No se ha podido loguear al customer que se acaba de registrar."); // Second check
+		Assert.notNull(authenticatedUser, "No se ha podido loguear al user que se acaba de registrar."); // Second check
 		
 		unauthenticate();
 
 	}
 	
 	/**
-	 * Negative test case: Registrarse como Customer con contraseña a null
+	 * Negative test case: Registrarse como User con contraseña a null
 	 * 		- Acción
 	 * 		+ Entrar el registro como anónimo
 	 * 		+ Rellenar los campos y la contraseña a null
@@ -132,12 +139,12 @@ public class UserServiceTest extends AbstractTest {
 	@Test(expected=IllegalArgumentException.class)
 	@Rollback(value = true)
 //	@Test 
-	public void testNewCustomerNullPassword() {
+	public void testNewUserNullPassword() {
 		// Declare variables
-		User customer;
+		User user;
 		UserAccount userAccount;
-//		Customer customerRegistered;
-//		Actor authenticatedCustomer;
+//		User userRegistered;
+//		Actor authenticatedUser;
 		
 		// Load objects to test
 		
@@ -146,40 +153,40 @@ public class UserServiceTest extends AbstractTest {
 		
 		
 		// Execution of test
-		customer = customerService.create();
+		user = userService.create();
 		
-		customer.setName("Nuevo");
-		customer.setSurname("Customer");
-		customer.setPhone("123456789");
+		user.setName("Nuevo");
+		user.setSurname("User");
+		user.setPhone("123456789");
 
-		userAccount = userAccountService.create("CUSTOMER");
+		userAccount = userAccountService.create("USER");
 		
-		userAccount.setUsername("nuevoCustomer");
+		userAccount.setUsername("nuevoUser");
 		userAccount.setPassword(null);
 		
-		customer.setUserAccount(userAccount);
+		user.setUserAccount(userAccount);
 		
-		customerService.saveFromOtherService(customer);
+		userService.saveFromOtherService(user);
 		
 		// Checks results
 //		authenticate("admin");
-//		Assert.isTrue(customerService.findAll().contains(customerRegistered), "El customer nuevo registrado no se encuentra entre los customers registrados en el sistema."); // First check
+//		Assert.isTrue(userService.findAll().contains(userRegistered), "El user nuevo registrado no se encuentra entre los users registrados en el sistema."); // First check
 //		unauthenticate();
 //		
-//		authenticate("customer1");
+//		authenticate("user1");
 //		
-//		authenticatedCustomer = actorService.findByPrincipal();
+//		authenticatedUser = actorService.findByPrincipal();
 //		
-//		Assert.notNull(authenticatedCustomer, "No se ha podido loguear al customer que se acaba de registrar."); // Second check
+//		Assert.notNull(authenticatedUser, "No se ha podido loguear al user que se acaba de registrar."); // Second check
 		
 		unauthenticate();
 
 	}
 	
 	/**
-	 * Negative test case: Registrarse como Customer estando identificado en el sistema
+	 * Negative test case: Registrarse como User estando identificado en el sistema
 	 * 		- Acción
-	 * 		+ Entrar el registro como customer
+	 * 		+ Entrar el registro como user
 	 * 		+ Rellenar los campos y la contraseña a null
 	 * 		+ Presionar en registrarse
 	 * 		- Comprobación
@@ -190,45 +197,348 @@ public class UserServiceTest extends AbstractTest {
 	@Test(expected=IllegalArgumentException.class)
 	@Rollback(value = true)
 //	@Test
-	public void testNewCustomerAsCustomer() {
+	public void testNewUserAsUser() {
 		// Declare variables
-		User customer;
+		User user;
 		UserAccount userAccount;
-//		Customer customerRegistered;
-//		Actor authenticatedCustomer;
+//		User userRegistered;
+//		Actor authenticatedUser;
 		
 		// Load objects to test
-		authenticate("customer1");
+		authenticate("user1");
 		
 		// Checks basic requirements
 		
 		
 		// Execution of test
-		customer = customerService.create();
+		user = userService.create();
 		
-		customer.setName("Nuevo");
-		customer.setSurname("Customer");
-		customer.setPhone("123456789");
+		user.setName("Nuevo");
+		user.setSurname("User");
+		user.setPhone("123456789");
 
-		userAccount = userAccountService.create("CUSTOMER");
+		userAccount = userAccountService.create("USER");
 		
-		userAccount.setUsername("nuevoCustomer");
-		userAccount.setPassword("nuevoCustomer");
+		userAccount.setUsername("nuevoUser");
+		userAccount.setPassword("nuevoUser");
 		
-		customer.setUserAccount(userAccount);
+		user.setUserAccount(userAccount);
 		
-		customerService.saveFromOtherService(customer);
+		userService.saveFromOtherService(user);
 		
 		// Checks results
 //		authenticate("admin");
-//		Assert.isTrue(customerService.findAll().contains(customerRegistered), "El customer nuevo registrado no se encuentra entre los customers registrados en el sistema."); // First check
+//		Assert.isTrue(userService.findAll().contains(userRegistered), "El user nuevo registrado no se encuentra entre los users registrados en el sistema."); // First check
 //		unauthenticate();
 //		
-//		authenticate("customer1");
+//		authenticate("user1");
 //		
-//		authenticatedCustomer = actorService.findByPrincipal();
+//		authenticatedUser = actorService.findByPrincipal();
 //		
-//		Assert.notNull(authenticatedCustomer, "No se ha podido loguear al customer que se acaba de registrar."); // Second check
+//		Assert.notNull(authenticatedUser, "No se ha podido loguear al user que se acaba de registrar."); // Second check
+		
+		unauthenticate();
+
+	}
+	
+	/**
+	 * Acme-Barter - Level C - 9.4
+	 * List and see the profile of the users who have registered to the system, which consists
+	 * of the following data: their name and surname, their social identities (if any),
+	 * the list of barters that they have registered, and the list of matches in which they are
+	 * involved.
+	 */
+	
+	/**
+	 * Positive test case: Listar el perfil de un User
+	 * 		- Acción
+	 * 		+ Entrar el registro como anónimo
+	 * 		+ Listar todos los usuarios del sistema
+	 * 		+ Listar el perfil de un usuario
+	 * 		- Comprobación
+	 * 		+ Comprobar que el número de usuarios del sistema traidos es el esperado.
+	 * 		+ Comprobar que el nombre y apellidos del usuario son los esperados
+	 * 		+ Comprobar que el número de SocialIdentities, Barters y Match, son los esperados.
+	 * 		+ Cerrar su sesión
+	 */
+	
+	@Test 
+	public void testListProfile() {
+		// Declare variables
+		Collection<User> users;
+		User userToList;
+		Collection<SocialIdentity> socialIdentities;
+		Collection<Barter> barters;
+		Collection<Match> matches;
+		
+		// Load objects to test
+		
+		
+		// Checks basic requirements
+		
+		
+		// Execution of test
+		authenticate("user2");
+		
+		users = userService.findAll();
+		
+		userToList = null;
+		
+		for(User u: users){
+			if(u.getUserAccount().getUsername().equals("user1")){
+				userToList = u;
+				break;
+			}
+		}
+		
+		Assert.notNull(userToList, "No existe el User que se pretende testear.");
+		
+		socialIdentities = socialIdentityService.findByUserId(userToList.getId());
+		
+		barters = barterService.findByUserNotCancelled(userToList.getId());
+		
+		matches = matchService.findAllUserInvolves(userToList.getId());
+		
+		// Checks results
+		Assert.isTrue(users.size() == 4, "El numero de usuarios del sistema traidos no es el esperado.");
+		
+		Assert.isTrue(userToList.getName().equals("Manolo"), "El nombre del usuario no es el esperado.");
+		
+		Assert.isTrue(userToList.getSurname().equals("Lopez"), "El apellido del usuario no es el esperado.");
+		
+		Assert.isTrue(socialIdentities.size() == 2, "El numero de socialIdentities del usuario no es el esperado.");
+		
+		Assert.isTrue(barters.size() == 2, "El numero de barters del usuario no es el esperado.");
+		
+		Assert.isTrue(matches.size() == 2, "El numero de matches del usuario no es el esperado.");
+		
+		unauthenticate();
+
+	}
+	
+	/**
+	 * Acme-Barter - Level B - 3.1
+	 * Follow another user.
+	 */
+	
+	/**
+	 * Acme-Barter - Level B - 3.2
+	 * List the users that he or she follows.
+	 */
+	
+	/**
+	 * Acme-Barter - Level B - 3.3
+	 * List the users who follow him or her.
+	 */
+	
+	/**
+	 * Positive test case: Seguir a otro usuario
+	 * 		- Acción
+	 * 		+ Autenticarse en el sistema
+	 * 		+ Listar los usuarios del sistema
+	 * 		+ Seguir a uno de ellos (Comprobar que no lo siga en ese momento)
+	 * 		- Comprobación
+	 * 		+ Comprobar que el otro usuario aparece en su lista de seguidos
+	 * 		+ Comprobar que su lista de seguidos contiene 1 usuario más que antes
+	 * 		+ Cerrar su sesión
+	 * 		+ Autenticarse en el sistema como el otro user al que ha seguido
+	 * 		+ Comprobar que el usuario que le siguió aparece ahora en su lista de seguidores
+	 * 		+ Comprobar que la lista de seguidores del usuario que le siguió tiene 1 usuario más
+	 * 		+ Cerrar su sesión
+	 */
+	
+	@Test 
+	public void testFollowOtherUser() {
+		// Declare variables
+		User user;
+		User userToFollow;
+		Collection<User> users;
+		Integer userFollowedSize;
+		Integer newUserFollowedSize;
+		Integer userToFollowFollowersSize;
+		Integer newUserToFollowFollowersSize;
+		
+		// Load objects to test
+		authenticate("user1");
+		user = userService.findByPrincipal();
+		
+		// Checks basic requirements
+		Assert.notNull(user, "El usuario no se ha logueado correctamente.");
+		
+		// Execution of test
+		users = userService.findAll();
+		
+		userToFollow = null;
+		
+		for(User u: users){
+			if(u.getUserAccount().getUsername().equals("user4")){
+				userToFollow = u;
+			}
+		}
+		
+		Assert.notNull(userToFollow, "No existe el usuario con el que se pretende testear.");
+		
+		userFollowedSize = userService.getFollowed().size();
+		unauthenticate();
+		
+		authenticate("user4");
+		userToFollowFollowersSize = userService.getFollowers().size();
+		unauthenticate();
+		
+		authenticate("user1");
+		Assert.isTrue(!userService.getFollowed().contains(userToFollow), "El usuario ya sigue al otro usuario al que se pretende seguir.");
+		
+		userService.followOrUnfollowById(userToFollow.getId());
+		
+		
+		// Checks results
+		newUserFollowedSize = userService.getFollowed().size();
+		Assert.isTrue(user.getFollowed().contains(userToFollow), "El usuario no sigue al usuario que ha intentado seguir.");
+		Assert.isTrue(userService.getFollowed().contains(userToFollow), "El usuario no sigue al usuario que ha intentado seguir.");
+		
+		Assert.isTrue(userFollowedSize + 1 == newUserFollowedSize, "El numero de seguidos por el usuario no es el mismo que antes mas 1.");
+		
+		unauthenticate();
+		
+		authenticate("user4");
+		
+		newUserToFollowFollowersSize = userService.getFollowers().size();
+		Assert.isTrue(userService.getFollowers().contains(user), "El usuario seguido no contiene al usuario que le pretendía seguir entre sus seguidos.");
+		
+		Assert.isTrue(userToFollowFollowersSize + 1 == newUserToFollowFollowersSize, "El numero de seguidores del usuario seguido no es el mismo que antes mas 1.");
+		
+		unauthenticate();
+
+	}
+	
+	/**
+	 * Positive test case: Dejar de seguir a otro usuario
+	 * 		- Acción
+	 * 		+ Autenticarse en el sistema
+	 * 		+ Listar los usuarios del sistema
+	 * 		+ Dejar de seguir a uno de ellos (Comprobar que lo siga en ese momento)
+	 * 		- Comprobación
+	 * 		+ Comprobar que el otro usuario ya no aparece en su lista de seguidos
+	 * 		+ Cerrar su sesión
+	 * 		+ Autenticarse en el sistema como el otro user al que ha dejado de seguir
+	 * 		+ Comprobar que el usuario que le siguió ya no aparece ahora en su lista de seguidores
+	 * 		+ Cerrar su sesión
+	 */
+	
+	@Test 
+	public void testUnfollowOtherUser() {
+		// Declare variables
+		User user;
+		User userToUnfollow;
+		Collection<User> users;
+		
+		// Load objects to test
+		authenticate("user1");
+		user = userService.findByPrincipal();
+		
+		// Checks basic requirements
+		Assert.notNull(user, "El usuario no se ha logueado correctamente.");
+		
+		// Execution of test
+		users = userService.findAll();
+		
+		userToUnfollow = null;
+		
+		for(User u: users){
+			if(u.getUserAccount().getUsername().equals("user2")){
+				userToUnfollow = u;
+			}
+		}
+		
+		Assert.notNull(userToUnfollow, "No existe el usuario con el que se pretende testear.");
+		
+		Assert.isTrue(userService.getFollowed().contains(userToUnfollow), "El usuario no sigue al otro usuario al que se pretende dejar de seguir.");
+		
+		userService.followOrUnfollowById(userToUnfollow.getId());
+		
+		
+		// Checks results
+		Assert.isTrue(!user.getFollowed().contains(userToUnfollow), "El usuario ya sigue al usuario que ha intentado dejar de seguir.");
+		Assert.isTrue(!userService.getFollowed().contains(userToUnfollow), "El usuario ya sigue al usuario que ha intentado dejar de seguir.");
+		
+		unauthenticate();
+		
+		authenticate("user2");
+		
+		Assert.isTrue(!userService.getFollowers().contains(user), "El usuario seguido contiene al usuario que le pretendía dejar de seguir entre sus seguidos.");
+		
+		unauthenticate();
+
+	}
+	
+	/**
+	 * Negative test case: Seguir/Dejar de seguir a ti mismo
+	 * 		- Acción
+	 * 		+ Autenticarse en el sistema
+	 * 		+ Listar los usuarios del sistema
+	 * 		+ Intentar seguir/dejar de seguir al mismo usuario
+	 * 		- Comprobación
+	 * 		+ Comprobar que salta una excepción del tipo: IllegalArgumentException
+	 * 		+ Cerrar su sesión
+	 */
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Rollback(value = true)
+//	@Test 
+	public void testFollowOrUnfollowYourself() {
+		// Declare variables
+		User user;
+		
+		// Load objects to test
+		authenticate("user1");
+		user = userService.findByPrincipal();
+		
+		// Checks basic requirements
+		Assert.notNull(user, "El usuario no se ha logueado correctamente.");
+		
+		// Execution of test
+		userService.followOrUnfollowById(user.getId());
+		
+		// Checks results
+		
+		unauthenticate();
+
+	}
+	
+	/**
+	 * Negative test case: Seguir a un admin (Otro actor que no sea de tipo User)
+	 * 		- Acción
+	 * 		+ Autenticarse en el sistema
+	 * 		+ Intentar seguir a un administrador
+	 * 		- Comprobación
+	 * 		+ Comprobar que salta una excepción del tipo: IllegalArgumentException
+	 * 		+ Cerrar su sesión
+	 */
+	
+	@Test(expected=IllegalArgumentException.class)
+	@Rollback(value = true)
+//	@Test 
+	public void testFollowAnAdmin() {
+		// Declare variables
+		User user;
+		Administrator admin;
+		
+		// Load objects to test
+		authenticate("admin");
+		admin = administratorService.findByPrincipal();
+		unauthenticate();
+		
+		authenticate("user1");
+		user = userService.findByPrincipal();
+		
+		// Checks basic requirements
+		Assert.notNull(user, "El usuario no se ha logueado correctamente.");
+		
+		// Execution of test
+		
+		userService.followOrUnfollowById(admin.getId());
+		
+		// Checks results
 		
 		unauthenticate();
 
@@ -246,7 +556,7 @@ public class UserServiceTest extends AbstractTest {
 		
 		// Load objects to test
 		authenticate("admin");
-		totalUsersInTest = customerService.findAll().size();
+		totalUsersInTest = userService.findAll().size();
 		
 		// Checks basic requirements
 				
@@ -271,7 +581,7 @@ public class UserServiceTest extends AbstractTest {
 		// Load objects to test
 		authenticate("admin");
 		
-		inTest = customerService.findAll();
+		inTest = userService.findAll();
 		limitCalendar = Calendar.getInstance();
 		limitCalendar.add(Calendar.MONTH, -1);
 		limitDate = limitCalendar.getTime();
@@ -312,7 +622,7 @@ public class UserServiceTest extends AbstractTest {
 		// Load objects to test
 		authenticate("admin");
 		
-		inTest = customerService.findAll();
+		inTest = userService.findAll();
 		
 		userBarters = new HashMap<User, Integer>();
 		maxBarters = 0;
@@ -358,7 +668,7 @@ public class UserServiceTest extends AbstractTest {
 		// Load objects to test
 		authenticate("admin");
 		
-		inTest = customerService.findAll();
+		inTest = userService.findAll();
 		
 		userBarters = new HashMap<User, Integer>();
 		maxBarters = 0;
@@ -406,7 +716,7 @@ public class UserServiceTest extends AbstractTest {
 		// Load objects to test
 		authenticate("admin");
 		
-		inTest = customerService.findAll();
+		inTest = userService.findAll();
 		
 		userBarters = new HashMap<User, Integer>();
 		maxBarters = 0;
@@ -473,7 +783,7 @@ public class UserServiceTest extends AbstractTest {
 		// Load objects to test
 		authenticate("admin");
 		
-		inTest = customerService.findAll();
+		inTest = userService.findAll();
 		
 		userBarters = new HashMap<User, Integer>();
 		maxBarters = 0;
